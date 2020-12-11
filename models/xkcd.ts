cc7d7c65e2d5ec40c0369f30d32415da38442118
Jan Willem Harmelink <janwillem@harmelink.net>
2020-12-11 21:53:53 +0100

    Initial commit

---
+++ models/xkcd.ts
@@ -0,0 +1,11 @@
+export interface XkcdResponse{
+    "data": {
+        number: number,
+        url: string,
+        title: string,
+        description: string,
+        year: number,
+        month: number,
+        day: number
+    }
+}
